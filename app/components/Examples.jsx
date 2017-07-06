ed5a4542dc3b6a8fb13318a59b1f67e8f28da2bb
JairoHernandez <jairomh@hotmail.com>
2017-07-06 15:17:59 -0500

    Rename examples page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,16 +1,19 @@
 var React = require('react');
 
-var Examples = React.createClass({
-    render: function() {
-        return (
-            <h3>Examples Component</h3>
-        );
-    }
-});
+// var Examples = React.createClass({
+//     render: function() {
+//         return (
+//             <h3>Examples Component</h3>
+//         );
+//     }
+// });
 
 var Examples = (props) => {
     return (
-         <h3>Examples Component</h3>
+        <div>
+            <h3>Examples Component</h3>
+            <p>Welcome to exaples page!</p>
+        </div>
     );
 };
 
