7037b129119141de1e76abe6ab92985f170726d1
JairoHernandez <jairomh@hotmail.com>
2017-07-06 23:26:22 -0500

    Working on Styling Examples Page

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -14,11 +14,16 @@ var Nav = require('Nav');
 // });
 
 var Main = (props) => {
+    debugger;
     return (
         <div>
             <Nav/>
-            <h1>Main Component</h1>
-            {props.children}
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    <h1>MAIN</h1>
+                    {props.children}
+                </div>
+            </div>            
         </div>
     );
 };
