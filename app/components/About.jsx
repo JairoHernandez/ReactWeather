60376249f3c1eaae565e955be37d78993e62eecc
JairoHernandez <jairomh@hotmail.com>
2017-07-09 15:01:53 -0500

    About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,15 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the About page</p>
+            
+            <h1 className="text-center">Author:</h1>
+            <p>
+               This is a weather application build on React. I have built this for the Complete React Web App Developer Course.
+            </p>
+            <a href="https://github.com/JairoHernandez/ReactWeather" target="_blank">github repo</a>
         </div>
     );
 };
