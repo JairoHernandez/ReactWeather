30cd7288413a227d82adcdec1a8c6321d187aa0b
JairoHernandez <jairomh@hotmail.com>
2017-07-10 20:47:57 -0500

    Style about page & link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,11 +5,22 @@ var About = (props) => {
     return (
         <div>
             
-            <h1 className="text-center">Author:</h1>
             <p>
-               This is a weather application build on React. I have built this for the Complete React Web App Developer Course.
+               This is a weather application build on React. 
+               I have built this for the Complete React Web App Developer Course.
             </p>
-            <a href="https://github.com/JairoHernandez/ReactWeather" target="_blank">github repo</a>
+            <p>Here are some of the tools I used:</p>
+            <ul>
+                <li>
+                    <a href="https://github.com/JairoHernandez/ReactWeather" target="_blank">React Github Repo</a>
+                    - This was the JavaScript framework used.
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> 
+                    - Used Open Weather to search for weather data by city name.
+
+                </li>
+            </ul>
         </div>
     );
 };
